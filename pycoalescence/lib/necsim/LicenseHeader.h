169e6432d4aab711439305074061e004fec71eee
Sam Thompson <thompsonsed@gmail.com>
2018-02-03 14:32:52 +0800

    Added necsim submodule

--- pycoalescence/lib/necsim/LicenseHeader.h
+++
@@ -1,3 +0,0 @@
-//This file is part of Coalescence_Sim project which is released under BSD-3 license.
-//See file **LICENSE.txt** or visit https://opensource.org/licenses/BSD-3-Clause) for full license details.
-
