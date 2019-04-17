870de42a936da01d2cf84f8843d2f9fb2c1f74e8
Sam Thompson <thompsonsed@gmail.com>
2019-04-17 15:00:38 +0100

    Updated to version 1.2.7a35

--- pycoalescence/__init__.py
+++ pycoalescence/__init__.py
@@ -3,7 +3,7 @@ pycoalescence provides the facilities for running spatially explicit neutral coa
 and performing basic analysis of the simulation outputs. The program requires necsim to function properly.
 
 """
-__version__ = "1.2.7a34"
+__version__ = "1.2.7a35"
 
 import logging
 
