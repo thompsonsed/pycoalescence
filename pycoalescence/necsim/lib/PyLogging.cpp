6613bd0b7557d2d5ddc7f0dc782f621e17e2f0fc
Sam Thompson <thompsonsed@gmail.com>
2021-03-17 20:58:43 +0000

    Added lib module and tidied up gitignore

---
+++ pycoalescence/necsim/lib/PyLogging.cpp
@@ -0,0 +1,50 @@
+//This file is part of necsim project which is released under MIT license.
+//See file **LICENSE.txt** or visit https://opensource.org/licenses/MIT) for full license details.
+/**
+ * @author Sam Thompson
+ * @file PyLogging.cpp
+ * @brief Routines for writing to Python logging module.
+ * @copyright <a href="https://opensource.org/licenses/MIT">MIT Licence.</a>
+ */
+#ifndef PYTHON_COMPILE
+#define PYTHON_COMPILE
+
+#include <Python.h>
+#include <string>
+#include <sstream>
+#include "../necsim/Logger.h"
+
+#include "PyLogger.h"
+#include "PyLogging.h"
+
+namespace necsim
+{
+    PyLogger* pyLogger = nullptr;
+
+    void setGlobalLogger(PyObject* logger, PyObject* log_function)
+    {
+        if(pyLogger != nullptr)
+        {
+            removeGlobalLogger();
+        }
+        pyLogger = new PyLogger();
+        if(logger == nullptr || log_function == nullptr)
+        {
+            string errmsg = "logger or log_function is nullptr when attempting to setGlobalLogger(). "
+                            "Please report this bug.";
+            PyErr_SetString(PyExc_SystemError, errmsg.c_str());
+        }
+        pyLogger->setLogger(logger);
+        pyLogger->setLogFunction(log_function);
+    }
+
+    void removeGlobalLogger()
+    {
+        if(pyLogger != nullptr)
+        {
+            delete pyLogger;
+        }
+        pyLogger = nullptr;
+    }
+}
+#endif
---
+++ pycoalescence/necsim/lib/PyLogging.h
@@ -0,0 +1,34 @@
+//This file is part of necsim project which is released under MIT license.
+//See file **LICENSE.txt** or visit https://opensource.org/licenses/MIT) for full license details.
+
+/**
+ * @author Sam Thompson
+ * @file PyLogging.h
+ * @brief Routines for writing to Python logging module.
+ * @copyright <a href="https://opensource.org/licenses/MIT">MIT Licence.</a>
+ */
+#ifndef PYLOGGING_H
+#define PYLOGGING_H
+
+#include "Python.h"
+#include <string>
+#include "../necsim/LogFile.h"
+#include "PyLogger.h"
+
+namespace necsim
+{
+    /**
+     * @brief Generates the global logger object and adds the logger and log functions to the Python logger.
+     *
+     * Each call to getGlobalLogger should be matched by a call to removeGlobalLogger
+     * @param logger the Python logger object to use
+     * @param log_function the Python logging function to use
+     */
+    void setGlobalLogger(PyObject* logger, PyObject* log_function);
+
+    /**
+     * @brief Safely deletes the global logger object.
+     */
+    void removeGlobalLogger();
+}
+#endif // PYLOGGING_H
